3752b4916e497510c705923294198a6c157a4e2f
Wagner Franchin <wagner.franchin@oracle.com>
2019-02-17 09:25:59 +0000

    Raft node IPs are now defined in an environment variable list and not with separated variables for each node.

--- run/run.sh
+++ run/run.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-sed -e "s/%RaftEnabled%/${RAFT}/; s/%ListenAddress%/${PORT}/; s/%RaftBind%/${BIND}/; s/%RaftNode1%/${NODE1}/; s/%RaftNode2%/${NODE2}/; s/%RaftNode3%/${NODE3}/" orchestrator-template-sqlite.conf.json > orchestrator.conf.json
+sed -e "s/%RaftEnabled%/${RAFT}/; s/%ListenAddress%/${PORT}/; s/%RaftBind%/${BIND}/; s/%RaftNodes%/${RAFT_NODES}/" orchestrator-template-sqlite.conf.json > orchestrator.conf.json
 
 cat orchestrator.conf.json
 
