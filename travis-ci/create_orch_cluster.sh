98d0a5656793bfda76c79c34960b12e98f7a4e14
Wagner Franchin <wagner.franchin@oracle.com>
2020-01-10 21:19:43 +0000

    Some changes in order to push the built image to docker hub

--- travis-ci/create_orch_cluster.sh
+++ travis-ci/create_orch_cluster.sh
@@ -4,5 +4,5 @@ for N in 1 2 3
 do docker run -d --name orchestrator$N --net orchnet -p "300$N":3000 \
   -e PORT=3000 -e BIND=orchestrator$N \
   -e RAFT_NODES='"orchestrator1","orchestrator2","orchestrator3"' \
-  orchestrator-raft:latest
+  wagnerfranchin/orchestrator-raft:1.0
 done
